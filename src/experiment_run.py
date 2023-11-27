b19337ec73ca1744c6325776097d5e68bca6879b
KostenokLisa <65138661+KostenokLisa@users.noreply.github.com>
2023-11-27 04:06:52 +0100

    Merged with the main template and added the preliminary experiments

--- src/experiment_run.py
+++ src/experiment_run.py
@@ -24,37 +24,51 @@ class Run:
 
         self.writer = SummaryWriter("runs/" + log_dir)
 
-    def loss(self, yh, y_data, x_physics, y_physics):
-        if self.physics_loss is None:
-            return self.data_loss(yh, y_data)
+    def loss(self, yh, y_data, x_physics, y_physics, on_epoch_end=False, iter=None):
+        loss = 0.0
+        if self.data_loss is not None:
+            data_loss = self.data_loss(yh, y_data)
+            if on_epoch_end:
+                self.__track_progress(iter, data_loss, "data_loss")
+            loss += data_loss
         
-        return self.data_loss(yh, y_data) + self.physics_loss(x_physics, y_physics)
+        if self.physics_loss is not None:
+            physics_loss = self.physics_loss( x_physics, y_physics)
+            if on_epoch_end:
+                self.__track_progress(iter, physics_loss, "physics_loss")
+            loss += physics_loss
+        
+        if on_epoch_end:
+            self.__track_progress(iter, loss, "total_loss")
+        return loss
 
-    def __track_progress(self, iter, loss):
-        self.writer.add_scalar("training loss", loss, iter)
-
-    def __step(self):
-        for x_physics in self.data_handler.physics_dataloader:
-            x_data = self.data_handler.x_data
-            y_data = self.data_handler.y_data
-
-            self.optimizer.zero_grad()
-
-            yh = self.model(x_data)
-            y_physics = self.model(x_physics)
-
-            loss = self.loss(yh, y_data, x_physics, y_physics)
-            loss.backward()
-
-            self.optimizer.step()
-            if self.scheduler is not None:
-                self.scheduler.step(loss.item())
-        return loss.item()
+    def __track_progress(self, iter, variable, variable_name):
+        self.writer.add_scalar(variable_name, variable, iter)
 
     def train(self, epochs):
         for i in tqdm(range(epochs)):
-            loss = self.__step()
-            self.__track_progress(i, loss)
+            for batch_idx, x_physics in enumerate(self.data_handler.physics_dataloader):
+                x_data = self.data_handler.x_data
+                y_data = self.data_handler.y_data
+
+                self.optimizer.zero_grad()
+
+                yh = self.model(x_data)
+                y_physics = self.model(x_physics)
+
+                if batch_idx == len(self.data_handler.physics_dataloader) - 1:
+                    # Tensorboard callbacks for the parts of loss at the end of each epoch
+                    loss = self.loss(yh, y_data, x_physics, y_physics, on_epoch_end=True, iter=i)
+                else:
+                    loss = self.loss(yh, y_data, x_physics, y_physics)
+                loss.backward()
+
+                self.optimizer.step()
+                if self.scheduler is not None:
+                    self.scheduler.step(loss.item())
+
+            # Tensorboard callback for learning rate
+            self.__track_progress(i, float(self.optimizer.param_groups[0]["lr"]), "learning_rate")
         self.__save_img()
 
     def __save_img(self):
--- src/models.py
+++ src/models.py
@@ -23,3 +23,28 @@ class FCN(torch.nn.Module):
     def forward(self, x):
         x = self.fcn(x)
         return x
+    
+
+class FCN_with_last_activation(torch.nn.Module):
+    """Defines a fully connected network"""
+
+    def __init__(self, module_dims, activation=nn.ELU, last_activation=nn.Softsign):
+        """
+        Construct FCN,
+        module_dims is a list with input dimension, hidden dimensions and output dimension
+        activation specifies the activation function
+        """
+        super().__init__()
+        modules = []
+        for i in range(len(module_dims) - 1):
+            modules.append(nn.Linear(module_dims[i], module_dims[i + 1]))
+            if i < len(module_dims) - 2:
+                modules.append(activation())
+            elif i == len(module_dims) - 2:
+                modules.append(last_activation())
+
+        self.fcn = nn.Sequential(*modules)
+
+    def forward(self, x):
+        x = self.fcn(x)
+        return x
